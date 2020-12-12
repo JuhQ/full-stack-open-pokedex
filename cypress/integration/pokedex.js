c1be99bb8b7b20afbe44e248e3d688e4fde91754
Juha Tauriainen <juha.pekka.tauriainen@cern.ch>
2020-12-12 20:02:45 +0200

    Add navigation test

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -6,4 +6,10 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('can navigate to a different page', function () {
+    cy.visit('http://localhost:5000')
+    cy.get('.list-item').eq(1).click()
+    cy.contains('chlorophyll')
+  })
 })
