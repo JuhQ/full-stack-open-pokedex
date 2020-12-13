3d642101bcfac8f1b8f12a37a1b633be655af61f
Juha Tauriainen <juha.pekka.tauriainen@cern.ch>
2020-12-13 18:52:40 +0200

    Add health check

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const packagejson = require('./package.json')
+
 const app = express()
 
 // Heroku dynamically sets a port
@@ -9,4 +11,12 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(packagejson.version)
 })
